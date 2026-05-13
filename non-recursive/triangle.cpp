38ad0c544ff61515876d2d3c2475e76d5068f8fb
AMokhtar51 <abdelrahman0ahmed1@gmail.com>
2026-05-13 18:48:51 +0300

    change sort algos

--- non-recursive/triangle.cpp
+++ non-recursive/triangle.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
+void insertionSort(vector<int> &a)
+{
+    int n = a.size();
+    for (int i = 1; i < n; i++)
+    {
+        int key = a[i];
+        int j = i - 1;
+        while (j >= 0 && a[j] > key)
+        {
+            a[j + 1] = a[j];
+            j--;
+        }
+        a[j + 1] = key;
+    }
+}
 int triangle(vector<int> a)
 {
     int n = a.size();
-    sort(a.begin(), a.end());
+    insertionSort(a);
     for (int i = 0; i < n - 2; i++)
     {
         if (a[i] + a[i + 1] > a[i + 2])
@@ -20,9 +34,14 @@ int main()
     int n;
     cout << "Enter size of the array: ";
     cin >> n;
-    cout << "Enter array to check for triplets: ";
-    vector<int> a(n);
-    for (int &x : a)
-        cin >> x;
-    cout << triangle(a) << "\n";
+    if (n < 3)
+        cout << "Cannot be a triangle." << endl;
+    else
+    {
+        cout << "Enter array to check for triplets: ";
+        vector<int> a(n);
+        for (int &x : a)
+            cin >> x;
+        cout << triangle(a) << "\n";
+    }
 }
--- recursive/triangle_rec.cpp
+++ recursive/triangle_rec.cpp
@@ -1,33 +1,100 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
-int triangle(const vector<int>& a,int i)
+void mergee(vector<int> &A, int p, int q, int r)
 {
-    if(i >= a.size() - 2)
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++)
+        L[i] = A[p + i];
+
+    for (int j = 0; j < n2; j++)
+        R[j] = A[q + 1 + j];
+
+    int i = 0, j = 0, k = p;
+
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            A[k] = L[i];
+            i++;
+        }
+        else
+        {
+            A[k] = R[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i < n1)
+    {
+        A[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        A[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vector<int> &A, int p, int r)
+{
+    if (p < r)
+    {
+        int q = (p + r) / 2;
+
+        mergeSort(A, p, q);
+
+        mergeSort(A, q + 1, r);
+
+        mergee(A, p, q, r);
+    }
+}
+
+int triangle(const vector<int> &A, int i)
+{
+    if (i >= A.size() - 2)
         return 0;
 
-    if(a[i] + a[i+1] > a[i+2])
+    if (A[i] + A[i + 1] > A[i + 2])
         return 1;
 
-    return triangle(a, i + 1);
+    return triangle(A, i + 1);
 }
 
 int main()
 {
     int n;
-    cout << "Enter size of the array: ";
+    cout << "Enter size of the array: " << endl;
     cin >> n;
-    if(n<3){
-        cout << "cannot be a triangle";
-    }else{
-    cout << "Enter array to check for triplets: ";
-    vector<int> a(n);
-    for (int &x : a)
-        cin >> x;
-    vector<int> b = a;
-    sort(b.begin(), b.end());
-    cout << triangle(b,0) << "\n";
+    if (n < 3)
+    {
+        cout << 0 << endl;
+        return 0;
     }
+    cout << "Enter array to check for triplets: " << endl;
+
+    vector<int> A(n);
+
+    for (int &x : A)
+        cin >> x;
+
+    vector<int> B = A;
+
+    mergeSort(B, 0, n - 1);
+
+    cout << triangle(B, 0) << endl;
+
+    return 0;
 }
